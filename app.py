d42e2175c192098c94ea269f3b86fe5f1694af4e
Zhu-Peter <gzhu@ualberta.ca>
2024-07-03 18:41:15 -0600

    bonus 1

--- app.py
+++ app.py
@@ -13,45 +13,51 @@
 # Create a function that takes two arguments, multiplies them and returns the result
 # Create a function that takes two arguments, divides them and returns the result
 
-def calculate(type):
-    if user_input == 1:
-        print("Please enter two numbers to add:")
-        ans = inputs()
-        return(ans[0] + ans[1])
 
-    elif user_input == 2:
-        print("Please enter two numbers to subtract:")
-        ans = inputs()
-        return(ans[0] - ans[1])
+while True: 
 
-    elif user_input == 3:
-        print("Please enter two numbers to multiply:")
-        ans = inputs()
-        return(ans[0] * ans[1])
+    def calculate(type):
+        if user_input == 1:
+            print("Please enter two numbers to add:")
+            ans = inputs()
+            return(ans[0] + ans[1])
 
-    elif user_input == 4:
-        print("Please enter two numbers to divide:")
-        ans = inputs()
-        return(ans[0] / ans[1])
+        elif user_input == 2:
+            print("Please enter two numbers to subtract:")
+            ans = inputs()
+            return(ans[0] - ans[1])
 
-def inputs():
+        elif user_input == 3:
+            print("Please enter two numbers to multiply:")
+            ans = inputs()
+            return(ans[0] * ans[1])
+
+        elif user_input == 4:
+            print("Please enter two numbers to divide:")
+            ans = inputs()
+            return(ans[0] / ans[1])
+
+    def inputs():
+        try:
+            num1 = int(input("Number 1: "))
+            num2 = int(input("Number 2: "))
+        except ValueError:
+            print("Please enter a valid number.")
+            num1 = 0
+            num2 = 0
+        
+        return [num1, num2]
+
     try:
-        num1 = int(input("Number 1: "))
-        num2 = int(input("Number 2: "))
+        user_input = int(input("Please select one of the following options: \n 1. Add two numbers \n 2. Subtract two numbers \n 3. Multiply two numbers \n 4. Divide two numbers \nPlease enter a number: "))
+
     except ValueError:
-        print("Please enter a valid number.")
-        num1 = 0
-        num2 = 0
-    
-    return [num1, num2]
+        user_input = 0
+        print("Please enter a valid option.")
 
-try:
-    user_input = int(input("Please select one of the following options: \n 1. Add two numbers \n 2. Subtract two numbers \n 3. Multiply two numbers \n 4. Divide two numbers \nPlease enter a number: "))
-
-except ValueError:
-    user_input = 0
-    print("Please enter a valid option.")
-
-else: 
-    print('Answer: ', calculate(user_input))
-
+    else: 
+        print('Answer: ', calculate(user_input))
+        stop = input("Would you like to continue? (y/n): ")
+        if stop == 'n':
+            break
+        
